e0667d415e38738ed31f7b82b101efc224e8954d
Joel, Pinto da Mata <joel.mata@iflavours.nl>
2017-12-27 09:16:17 +0100

    Initial commit

---
+++ src/ScreeningTest.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ScreeningTest {
+
+    public static void main(String[] args) {
+
+        List<Integer> data1 = Arrays.asList(1, 4, 7);
+        List<Integer> data2 = Arrays.asList(123, -2, 477, 3, 14, 6551);
+
+        ScreeningTest obj = new ScreeningTest();
+
+        int result = obj.fold(data1);
+        System.out.println(result);
+
+        int yourAnswer = obj.fold(data2); //what is the answer for this one???
+        System.out.println(yourAnswer);
+    }
+
+    private int fold(List<Integer> input) {
+        if(input.size() == 1)
+            return input.get(0);
+        else {
+            int current = input.get(0);
+            List<Integer> inputAux = input.subList(1, input.size())
+                    .stream().map(x ->
+                        x + current
+                    ).collect(Collectors.toList());
+            return fold(inputAux);
+        }
+    }
+}
